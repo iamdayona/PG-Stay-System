7c4e8a1bb3660212d8dbcc0701c61a8c9cc5ab70
iamdayona <dayonasuby>
2026-02-10 19:45:44 +0530

    Updated App.jsx and Main.jsx and added new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import Home from './pages/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import OwnerDashboard from "./pages/OwnerDashboard";
+import OwnerProfile from "./pages/OwnerProfile";
+import PGSManagement from "./pages/PGSManagement";
+import OwnerApplications from "./pages/OwnerApplications";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* Owner Routes */}
+        <Route path="/owner/dashboard" element={<OwnerDashboard />} />
+        <Route path="/owner/profile" element={<OwnerProfile />} />
+        <Route path="/owner/pgsmanagement" element={<PGSManagement />} />
+        <Route path="/owner/applications" element={<OwnerApplications />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ui/button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ children, type = "button", onClick, className = "" }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 transition ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export { Button };
