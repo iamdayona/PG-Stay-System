e11f88d23bc2ddd1ef28432fa53f8ff091b96593
Aromalharikumar <145173059+Aromalharikumar@users.noreply.github.com>
2026-02-12 22:39:33 +0530

    add pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,6 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
       <RoleProvider>
         <App />
       </RoleProvider>
