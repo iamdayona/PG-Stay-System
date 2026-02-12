953b18d57ee3e6d8c01b884b863c4d330f1bb13c
ageeshcyriac <ageeshcyriacbaiju33@gmail.com>
2026-02-12 23:23:38 +0530

    udated app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,38 +2,43 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import UserProfile from "./pages/UserProfile";
+
+import AdminVerifyMonitor from "./pages/AdminVerifyMonitor";
+import AdminDashboard from "./pages/AdminDashboard";
+
 import OwnerDashboard from "./pages/OwnerDashboard";
+import OwnerNotifications from "./pages/OwnerNotifications";
 import OwnerProfile from "./pages/OwnerProfile";
 import PGSManagement from "./pages/PGSManagement";
 import OwnerApplications from "./pages/OwnerApplications";
+
+import TenantProfile from "./pages/TenantProfile";
 import TenantFindPGs from "./pages/TenantFindPGs";
 import TenantApplications from "./pages/TenantApplications";
 import TenantNotifications from "./pages/TenantNotifications";
-
-import OwnerNotifications from "./pages/OwnerNotifications";
-import AdminVerifyMonitor from "./pages/AdminVerifyMonitor";
-import AdminDashboard from "./pages/AdminDashboard";
 import TenantDashboard from "./pages/TenantDashboard";
+
+
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-
-      {/* Tenant Routes */}
-      <Route path="/tenant/findpgs" element={<TenantFindPGs />} />
-      <Route path="/tenant/applications" element={<TenantApplications />} />
-      <Route path="/tenant/notifications" element={<TenantNotifications />} />
+      
       {/* Login */}
       <Route path="/login" element={<Login />} />
-      <Route path="/userprofile" element={<UserProfile />} />
+
 
       {/* Admin Routes */}
       <Route path="/admin/dashboard" element={<AdminDashboard />} />
       <Route path="/admin/verifymonitor" element={<AdminVerifyMonitor />} />
 
        {/* Tenant Routes */}
+      <Route path="/tenant/profile" element={<TenantProfile />} />
       <Route path="/tenant/dashboard" element={<TenantDashboard />} />
+      <Route path="/tenant/findpgs" element={<TenantFindPGs />} />
+      <Route path="/tenant/applications" element={<TenantApplications />} />
+      <Route path="/tenant/notifications" element={<TenantNotifications />} />
 
       {/* Owner Routes */}
       <Route path="/owner/dashboard" element={<OwnerDashboard />} />
