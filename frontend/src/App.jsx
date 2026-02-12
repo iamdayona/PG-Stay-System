8eadb62f76f8bd7a65352484b9bb83e846e3005d
ageeshcyriac <ageeshcyriacbaiju33@gmail.com>
2026-02-12 15:48:30 +0530

    updated pages1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 import OwnerDashboard from "./pages/OwnerDashboard";
 import OwnerProfile from "./pages/OwnerProfile";
 import PGSManagement from "./pages/PGSManagement";
 import OwnerApplications from "./pages/OwnerApplications";
-import AdminVerifyMonitor from "./pages/AdminVerifyMonitor";
-import AdminDashboard from "./pages/AdminDashboard";
-
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-      {/* Login */}
-      <Route path="/login" element={<Login />} />
-
-      {/* Admin Routes */}
-      <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      <Route path="/admin/verifymonitor" element={<AdminVerifyMonitor />} />
-
-      {/* Owner Routes */}
-      <Route path="/owner/dashboard" element={<OwnerDashboard />} />
-      <Route path="/owner/profile" element={<OwnerProfile />} />
-      <Route path="/owner/pgsmanagement" element={<PGSManagement />} />
-      <Route path="/owner/applications" element={<OwnerApplications />} />
-    </Routes>
+        {/* Owner Routes */}
+        <Route path="/owner/dashboard" element={<OwnerDashboard />} />
+        <Route path="/owner/profile" element={<OwnerProfile />} />
+        <Route path="/owner/pgsmanagement" element={<PGSManagement />} />
+        <Route path="/owner/applications" element={<OwnerApplications />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
